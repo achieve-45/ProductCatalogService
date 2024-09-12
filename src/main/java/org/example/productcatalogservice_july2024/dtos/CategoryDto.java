8819a37ebee178599a4573bea4534ee75eea6266
achieve-45 <pupuachieve7@gmail.com>
2024-09-12 20:21:21 +0530

    added codes 2

--- src/main/java/org/example/productcatalogservice_july2024/dtos/CategoryDto.java
+++ src/main/java/org/example/productcatalogservice_july2024/dtos/CategoryDto.java
@@ -6,12 +6,25 @@ import org.example.productcatalogservice_july2024.models.Product;
 
 import java.util.List;
 
+/**
+ * CategoryDto is a Data Transfer Object for Category entities.
+ * It includes category details such as id, name, and description.
+ */
 @Setter
 @Getter
 public class CategoryDto {
+    /**
+     * The unique identifier of the category.
+     */
     private Long id;
 
+    /**
+     * The name of the category.
+     */
     private String name;
 
+    /**
+     * The description of the category.
+     */
     private String description;
 }
